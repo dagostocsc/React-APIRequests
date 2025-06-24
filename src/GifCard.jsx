77fa26d6caf36daab0dac4760cf6325b196de0e6
Dagostocsc <Deborahagosto19@gmail.com>
2025-06-24 12:33:49 -0700

    Debbie Spagehtti

--- src/GifCard.jsx
+++ src/GifCard.jsx
@@ -4,25 +4,24 @@ const GetGif = async () => {
   const GIFAPIKEY = "https://api.giphy.com/v1/gifs/random?api_key=3Xt6KP7aUbnzyRIIlngcOzpV95VCfD0L&tag=&rating=g";
 
   try {
-    const response = await fetch(GIFAPIKEY);
-    if (!response.ok) {
-      throw new Error(`Error: ${response.status}`);
+    const response = await fetch(GIFAPIKEY); //here is the url meant to retrieve the api key
+    if (!response.ok) { 
+      throw new Error(`Error: ${response.status}`); //if the responce is not approved/satisfied it will throw an error
     }
-    const json = await response.json();
-  } catch (error) {
-    console.error(error.message);
+    const json = await response.json(); //then we convert it to the json but i dont undersnad how it compiles or works
+  } catch (error) { //catch I'm assuming is like throw where it will give an error before crashing any code
+    console.error(error.message);  //outputting the error message
     return "we dont know yet"
   }
-
-  function useEffect() =>
-  {
-    fetch(GIFAPIKEY)
-  }
+ useEffect(() => { //I dont remeber fully how the Useeffect works but I understand we have to call it to retieve 
+  //I guess whatever other relevant info within the functiono
+    fetchGetGif();
+  }, []);
 };
 
-const GifCard = () =>
+const GifCard = () => // 
   {
-    
+    const [,"something"] = useState 
   };
 
 export default GifCard;
